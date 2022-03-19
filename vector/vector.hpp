2e53075a96ba4a39ee4dc4c5a4b03aa43f5282ea
Amine <amine@josmac.home>
2022-03-19 19:38:01 +0100

    Avancement

--- vector/vector.hpp
+++ vector/vector.hpp
@@ -6,7 +6,7 @@
 /*   By: amine <amine@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/19 15:29:31 by amine             #+#    #+#             */
-/*   Updated: 2022/03/19 16:15:35 by amine            ###   ########.fr       */
+/*   Updated: 2022/03/19 19:37:51 by amine            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,18 +29,28 @@ namespace ft
 			typedef	size_t						size_type;
 
 
-			explicit	vector(void) : value_type(0), allocator_type(0), difference_type(0), size_type(0), pointer(NULL) {};
-			explicit	vector (size_type n, const value_type &val = value_type(), const allocator_type &alloc = allocator_type()) {};
+			explicit vector (const allocator_type& alloc = allocator_type()) : _alloc(alloc), _size(0), _capacity(0);
+			explicit	vector (size_type n, const value_type &val = value_type(), const allocator_type &alloc = allocator_type()) : _size(n), _capacity(n), _alloc(alloc) // Fill constructor
+			{
+				_array = _alloc.allocate(_size);
+				for (int i = 0; i < n; ++i)
+					_alloc.construct(&_array[i], val);
+			};
 			template <class InputIterator>
 			vector (InputIterator first, InputIterator last, const allocator_type& alloc = allocator_type()) {};
 
-			vector(vector const & src);
-			~vector(void);
+			vector (const vector &x) { *this = x; };
+			~vector(void) {};
 	
 			vector&     operator=(vector const & src);
 
+			size_type			max_size(void) {return _alloc.max_size(); }
+
 		private :
-			
+			size_type						_size;
+			size_type						_capacity; //Always double capacity on array size max reached and need to realloc
+			pointer							_array;
+			allocator_type					_alloc;
 	};
 
 
